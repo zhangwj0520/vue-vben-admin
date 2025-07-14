5b232141994020ab2846cab7816c24332ac90407
zhangweijie <zhangwj0520@hotmail.com>
2025-07-14 15:43:04 +0800

    demo

--- app/src/router/routes/modules/demos.ts
+++ app/src/router/routes/modules/demos.ts
@@ -21,6 +21,14 @@ const routes: RouteRecordRaw[] = [
         path: '/demos/element',
         component: () => import('#/views/demos/element/index.vue'),
       },
+      // {
+      //   meta: {
+      //     title: $t('demos.icon'),
+      //   },
+      //   name: 'NaiveDemos',
+      //   path: '/demos/icons',
+      //   component: () => import('#/views/demos/element/index.vue'),
+      // },
       {
         meta: {
           title: $t('demos.form'),
---
+++ app/src/views/demos/icons/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineOptions({ name: 'DemoIcons' });
+</script>
+
+<template>
+  <div>index</div>
+</template>
+
+<style lang="css" scoped></style>
